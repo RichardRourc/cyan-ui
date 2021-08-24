a171a406ce127c72a3ab70953d2e77362b81c2a5
chennaiyuan <chennaiyuan@chennaiyuandeMac-mini.local>
2021-08-24 18:49:49 +0800

    feat eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'script',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {},
+}
